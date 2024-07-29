75b609c32663a375132a0f1df3f042cf0bc50839
shriyashree24 <167524633+shriyashree24@users.noreply.github.com>
2024-07-29 21:58:56 +0530

    Added sg.tf

---
+++ sg.tf
@@ -0,0 +1,11 @@
+resource "aws_security_group" "eks_cluster_sg" {
+  name_prefix = "eks-cluster-sg-"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
